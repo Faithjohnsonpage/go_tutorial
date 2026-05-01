6687598c1796c1ab91d468928b68055175ee0760
Faith Johnson <ermac_4life@yahoo.com>
2026-05-01 12:05:04 +0100

    Added documentation to functions

--- package_example/do-format/format.go
+++ package_example/do-format/format.go
@@ -1,7 +1,9 @@
+// Package format provides functions for converting data types into formatted strings.
 package format
 
 import "fmt"
 
+// Number takes an integer and returns a string formatted as "The number is %d".
 func Number(num int) string {
 	return fmt.Sprintf("The number is %d", num)
 }
--- package_example/math/math.go
+++ package_example/math/math.go
@@ -1,5 +1,7 @@
+// Package math provides basic mathematical utility functions.
 package math
 
+// Double returns the result of multiplying the input integer by two.
 func Double(a int) int {
 	return a * 2
 }
