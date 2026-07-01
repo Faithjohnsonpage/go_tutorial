3e1458367903b1916e188044e35a842ab2b608d8
Faith Johnson <ermac_4life@yahoo.com>
2026-07-01 08:37:18 +0100

    second_solution_to_deadlock.go

---
+++ concurrency/second_solution_to_deadlock.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+func main() {
+	ch1 := make(chan int)
+	ch2 := make(chan int)
+	go func() {
+		inGoroutine := 1
+		ch1 <- inGoroutine
+		fromMain := <-ch2
+		fmt.Println("goroutine:", inGoroutine, fromMain)
+	}()
+	inMain := 2
+	var fromGoroutine int
+	select {
+	case ch2 <- inMain:
+	case fromGoroutine = <-ch1:
+	}
+	fmt.Println("main:", inMain, fromGoroutine)
+}
